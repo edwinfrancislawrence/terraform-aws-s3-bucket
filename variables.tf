b7c7ec28a794ed405ef3b7316d0d52df642a8008
edwinfrancislawrence <edwinfrancislawrence@gmail.com>
2026-06-11 10:57:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -106,7 +106,7 @@ variable "region" {
   default     = null
 }
 
-variable "bucket" {
+variable "bucket name" {
   description = "(Optional, Forces new resource) The name of the bucket. If omitted, Terraform will assign a random, unique name."
   type        = string
   default     = null
